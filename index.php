88aded15b9a94b067f4a839c7e007adc0140b6cf
Mihai BUDECI <mbudeci@pentalog.com>
2018-11-26 13:03:13 +0200

    test1

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+<title>Pentagon</title>
+</head>
+<body>
+<h1 align="center" style="color:Orange;">Pentalog</h1>
+<p>Acces the link bellow:</p>
+<a href="https://p5.pentalog.fr/">Pentalog<br></br></a>
+
+<!-- Initialele -->
+<form action="/action_page.php">
+  <fieldset>
+    <legend style="color:Red;">Personal information:</legend>
+    First name:<br>
+    <input type="text" name="firstname" value="Unknown"><br>
+    Last name:<br>
+    <input type="text" name="lastname" value="Unknown"><br><br>
+  </fieldset>
+</form>
+
+</body>
+</html>
+
+<html>
+<head>
+<script>
+function myFunction() {
+    document.getElementById("demo").innerHTML = "loh:)";
+}
+</script>
+</head>
+
+<body>
+
+<h2>Click for a surprise</h2>
+
+<p id="demo">Salut!</p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+</body>
+</html> 
+
+
